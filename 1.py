9bb13bfa0be9fefb5eb0c0497e309b05e07612a5
esta0 <rlaehgus2076@gmail.com>
2022-01-21 15:55:41 +0900

    [1]~~~~

---
+++ 1.py
@@ -0,0 +1,7 @@
+a = "편안하게 재미있게 파이썬을 공부해라"
+print(a)
+name = input("이름을 적으세요")
+message = " 안녕하세요 " + name + "님"
+print(message)
+b = "안녕하세요 반갑습니다 '코딩'을 하는법을 배우고있어요 :)"
+print(b)
--- 연습.py
+++ 연습.py
@@ -1,1 +1,4 @@
 print("Hello GitHub")
+'''
+dsfasdfdsafdsfds
+'''
